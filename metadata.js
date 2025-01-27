7db3ca58b31da823cf338abe22f55bb39fefc63a
haradav <116386891+haradav@users.noreply.github.com>
2025-01-27 18:09:24 +0900

    Add files via upload

---
+++ metadata.js
@@ -0,0 +1,5 @@
+{
+    "name": "Albert Einstein",
+    "symbol": "EMCII",
+    "image": "https://raw.githubusercontent.com/haradav/AlbertEinstein/refs/heads/main/albert-einstein-sticks-out-his-tongue-when-asked-by-news-photo-1681316749.avif"
+  }
