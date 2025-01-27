a4170d762efe5f13c169cf2e1410e00b09d7d4ab
haradav <116386891+haradav@users.noreply.github.com>
2025-01-27 18:36:45 +0900

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -1,5 +1,6 @@
 {
     "name": "Albert Einstein",
     "symbol": "EMCII",
+        "description" : "KNOWLEDGE TOKEN",
     "image": "https://raw.githubusercontent.com/haradav/AlbertEinstein/refs/heads/main/albert-einstein-sticks-out-his-tongue-when-asked-by-news-photo-1681316749.avif"
   }
